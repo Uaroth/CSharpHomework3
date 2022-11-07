351fee30a362df92cc52b7588bbfbdc9cf5d35e6
Sergey <melnikov_s_a@mail.ru>
2022-11-07 10:53:31 +0300

    Example3

---
+++ Example3/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает на вход число (N) 
+// и выдаёт таблицу кубов чисел от 1 до N.
+
+Console.WriteLine("Здравствуйте. Введите число N");
+int numberN = Convert.ToInt32(Console.ReadLine());
+
+void AllCubes(int number)
+{
+    for (int i = 1; i <= number; i++)
+    {
+        int result = i * i * i;
+        Console.Write($"{result}, ");
+    }
+}
+AllCubes(numberN);
